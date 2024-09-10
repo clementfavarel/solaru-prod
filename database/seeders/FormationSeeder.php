29f65f1cc42d0dc21841384a89969b69f5025b93
clementfavarel <pl.kode.pro@gmail.com>
2024-09-10 11:22:41 +0000

    created Formation seeder

---
+++ database/seeders/FormationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Formation;
+use Illuminate\Database\Seeder;
+
+class FormationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
